d50486a8541c90e3602b06b9091dde0e6bb818d1
narcissus313 <srz.shl@gmail.com>
2023-05-10 09:56:33 +0330

    articles started

--- public/javascripts/renderHome.js
+++ public/javascripts/renderHome.js
@@ -6,7 +6,6 @@ if (!!isLoggedIn) {
    <a class="btn btn-outline-danger ms-5 me-2" role="tab" id="logoutBtn" href="/user/logout">Logout</a>
 `;
 } else {
-	console.log("f");
 	homeNav.innerHTML = `
    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
    <button class="btn btn-outline-primary" type="submit">Search</button>
